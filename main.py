f6a881e896c044f55539238a776ef71c4236ba7f
Samiur <samiurkh1n@gmail.com>
2016-08-01 16:20:13 -0400

    Added handlers to main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 import jinja2
 import os
 from google.appengine.api import users
-#import ndb classes from outside
+#import ndb models from outside
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
