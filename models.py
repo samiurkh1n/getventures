325af4ba094ff3b8525fc30f8f740b30e8bb89ad
Samiur <samiurkh1n@gmail.com>
2016-08-01 17:18:39 -0400

    Added database model file

---
+++ models.py
@@ -0,0 +1,13 @@
+'''
+Models.py
+
+This file contains all the data models to be used in the main application
+Uses the datastore non-relational database of Google App Engine.
+'''
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+class User:
+
+class Meetup:
