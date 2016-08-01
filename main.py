d3d48f46cb784010f288aaef738a2b28201ebbb8
Samiur <samiurkh1n@gmail.com>
2016-08-01 16:16:37 -0400

    Added jinja to app config file

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import webapp2
 import jinja2
 import os
 from google.appengine.api import users
+#import ndb classes from outside
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
