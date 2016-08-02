decc30beef83f82f8cfaeb0925fd9b1479dbc01f
Samiur <samiurkh1n@gmail.com>
2016-08-01 22:38:58 -0400

    Created models and imported them into main

--- models.py
+++ models.py
@@ -8,6 +8,18 @@ Uses the datastore non-relational database of Google App Engine.
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
-class User:
-
-class Meetup:
+class User(ndb.Model):
+    email = ndb.StringProperty(required=True)
+    first_name = ndb.StringProperty(required=False)
+    last_name = ndb.StringProperty(required=False)
+    lastknown_latitude = ndb.FloatProperty(required=True)
+    lastknown_longitude = ndb.FloatProperty(required=True)
+    
+class Meetup(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    description = ndb.StringProperty(required=False)
+    event_admin = ndb.StringProperty(required=True)
+    event_participants = ndb.StringProperty(required=True)
+    type_of_places = ndb.StringProperty(required=True)
+    recommendation_map = ndb.StringProperty(required=False)
+    recommendation_list = ndb.StringProperty(required=False)
