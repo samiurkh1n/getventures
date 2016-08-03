6ce6e1f59be0f186276c75890a2fc27fb6a2baaa
Daryle <demouser@MacBook-Pro.local>
2016-08-03 10:49:28 -0400

    riccardos code in js and outputs

--- scripts/website.js
+++ scripts/website.js
@@ -2,24 +2,52 @@ var map;
 var service;
 var infowindow;
 
+// function initialize() {
+//   var newyork = new google.maps.LatLng(40,-74);
+//
+//   map = new google.maps.Map(document.getElementById('map-canvas'), {
+//       center: newyork,
+//       zoom: 8
+//     });
+//
+//   var request = {
+//     location: newyork,
+//     radius: '500',
+//     query: 'restaurant'
+//   };
+//
+//
+//   google.maps.event.addDomListener(window, 'load', initialize);
+//   service = new google.maps.places.PlacesService(map);
+//   service.textSearch(request, callback);
+// }
+
+// Initialization for the Maps APIs
 function initialize() {
-  var newyork = new google.maps.LatLng(40,-74);
-
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: newyork,
-      zoom: 15
-    });
-
-  var request = {
-    location: newyork,
-    radius: '500',
-    query: 'restaurant'
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
+   var mapOptions = {
+     center: { lat: -34.397, lng: 150.644},
+     zoom: 8
+   };
+   map = new google.maps.Map(document.getElementById('map-canvas'),
+       mapOptions);
 }
 
+// The google Map frame has a number of events like any other HTML element.
+// Here we set an handler to inialize the map when the map loads. You can
+// add listeners for "click", "move" and so on...
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
+
+
+
+
+//Technical map options here
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
